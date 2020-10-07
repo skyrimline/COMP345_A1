1826fed80ab8807c3ec2b097f9199a51508fc1f1
skyrimline <32933665+skyrimline@users.noreply.github.com>
2020-10-07 21:53:48 +0800

    Update Orders.h

--- Orders.h
+++ Orders.h
@@ -1,5 +1,29 @@
+#pragma once
 #include <iostream>
+using namespace std;
+class OrderList{
+	public:
+		OrderList();
+		OrderList(string);
+		OrderList(OrderList &OrderList, string olName);
+		OrderList& operator = (const OrderList &p)
+	{
 
-class Orders {
+		return *this;
+	}
+	
+};
+class Order : public OrderList {
+	public:
+		Order();
+		Order(string);
+		Order(Order &Order, string oName);
+		Order& operator = (const Order &p)
+		{
 
+			return *this;
+		}
+
+	private:
+		string oName;
 };
