42e0d940dd9f12165f8101bfa897938a14721b27
skyrimline <32933665+skyrimline@users.noreply.github.com>
2020-10-13 08:27:34 +0800

    Update Orders.cpp

--- Orders.cpp
+++ Orders.cpp
@@ -1,6 +1,6 @@
-#pragma once
 #include <iostream>
 #include <list>
+#include "Orders.h"
 using namespace std;
 
 
@@ -8,25 +8,47 @@ class Order
 {
 public:
 	Order();
-	Order(Order, string);
-	Order& operator = (const Order);
-	bool validate(Order);
-	void execute(Order);
+	Order(Order &order, string oname);
+	Order()
+	{
+	}
+	Order& operator = (const Order &order)
+	{
+		return *this;
+	};
+	virtual bool validate(Order p_order)
+	{
+			return 0;
+	};
+	virtual void execute(Order p_order);
 
 private:
 	string name;
 };
-
 class OrderList
 {
 	static list <Order> order;
 public:
-	OrderList() {
+	OrderList();
+	OrderList(const OrderList &orderlist)
+	{
+
 	};
-	OrderList(OrderList &OrderList);
-	OrderList& operator = (const OrderList &p);
-	void move(Order);
-	void remove(Order);
+	OrderList& operator = (const OrderList &orderlist)
+	{
+		return *this;
+	};
+	friend void move(Order order)
+	{
+		list.push_back(order);
+		cout << "Player has added Order " << order.name << " to the OrderList" endl;
+
+	};
+	friend void remove()
+	{
+		list.pop_back();
+
+	};
 };
 
 class deploy : public Order 
@@ -35,9 +57,21 @@ public:
 	deploy();
 	deploy(deploy &deploy);
 	deploy& operator = (const deploy &p);
-
+	friend bool validate(Order p_order)
+	{	
+		if (p_order.name == this->name)
+			return 1;
+		else
+			return 0;
+	};
+	 void execute(Order p_order) {
+		if (validate(p_order))
+			cout << "Player1 has deployed their army." << endl;
+		else
+			cout << "" << endl;
+	};
 private:
-	string name;
+	string name = "deploy";
 };
 
 class advance : public Order 
@@ -46,10 +80,23 @@ public:
 	advance();
 	advance(advance &advance);
 	advance& operator = (const advance &p);
-
+	friend bool validate(Order p_order)
+	{
+		if (p_order.name == this->name)
+			return 1;
+		else
+			return 0;
+	};
+	 void execute(Order p_order) {
+		if (validate(p_order))
+			cout << "Player1 has advanced their army." << endl;
+		else
+			cout << "" << endl;
+	 };
+	
 private:
-	string name;
-};	
+	string name = "advance";
+};
 
 class bomb : public Order 
 {
@@ -57,9 +104,22 @@ public:
 	bomb();
 	bomb(bomb &bomb);
 	bomb& operator = (const bomb &p);
+	friend bool validate(Order p_order)
+	{
+		if (p_order.name == this->name)
+			return 1;
+		else
+			return 0;
+	};
+	void execute(Order p_order) {
+		if (validate(p_order))
+			cout << "Player1 has bombed." << endl;
+		else
+			cout << "" << endl;
+	};
 
 private:
-	string name;
+	string name = "bomb";
 };
 
 class blockade : public Order 
@@ -68,9 +128,22 @@ public:
 	blockade();
 	blockade(blockade &blockade);
 	blockade& operator = (const blockade &p);
+	friend bool validate(Order p_order)
+	{
+		if (p_order.name == this->name)
+			return 1;
+		else
+			return 0;
+	};
+	void execute(Order p_order) {
+		if (validate(p_order))
+			cout << "Player1 has blockaded their territory." << endl;
+		else
+			cout << "" << endl;
+	};
 
 private:
-	string name;
+	string name = "blockade";
 };
 
 class airlift : public Order 
@@ -79,9 +152,22 @@ public:
 	airlift();
 	airlift(airlift &airlift);
 	airlift& operator = (const airlift &p);
+	friend bool validate(Order p_order)
+	{
+		if (p_order.name == this->name)
+			return 1;
+		else
+			return 0;
+	};
+	void execute(Order p_order) {
+		if (validate(p_order))
+			cout << "Player1 has airlifted his army." << endl;
+		else
+			cout << "" << endl;
+	};
 
 private:
-	string name;
+	string name = "airlift";
 };
 
 class negotiate : public Order 
@@ -90,7 +176,28 @@ public:
 	negotiate();
 	negotiate(negotiate &negotiate);
 	negotiate& operator = (const negotiate &p);
+	friend bool validate(Order p_order)
+	{	
+		if (p_order.name == this->name)
+			return 1;
+		else
+			return 0;
+	};
+	void execute(Order p_order) {
+		if (validate(p_order))
+			cout << "Player1 has used diplomacy." << endl;
+		else
+			cout << "" << endl;
+	};
 
 private:
-	string name;
+	string name = "negotiate";
 };
+
+
+
+int main() {
+
+
+	return 0;
+}
