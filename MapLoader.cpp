8ad9bc95685830ec334b335e74fbead3e1219a12
YutaoF <f18177216662@gmail.com>
2020-10-15 23:53:47 -0400

    Some example map and changes in mapLoader

--- MapLoader.cpp
+++ MapLoader.cpp
@@ -9,21 +9,21 @@ using namespace std;
 Map MapLoader::readMap(string mapFile) {
 		try {
 			Map map;
-			ifstream dom(mapFile);
+			ifstream file(mapFile);
 			vector <string> continentsList;
 			vector<string> territoryList;
-			string L;
+			string str;
 			//moving to continents
-			while (getline(dom, L) && L != "[continents]\r") {}
-			while (getline(dom, L) && L != "\r") {
-				string continent = L.substr(0, L.find(" "));
+			while (getline(file, str) && str != "[continents]") {}
+			while (getline(file, str) && !str.empty()) {
+				string continent = str.substr(0, str.find(" "));
 				continentsList.push_back(continent);
 				map.addContinent(new Continent(continent));
 			}
 			//moving to Territory
-			while (getline(dom, L) && L != "[contries]\r") {}
-			while (getline(dom, L) && L != "\r") {
-				stringstream st(L);
+			while (getline(file, str) && str != "[countries]") {}
+			while (getline(file, str) && !str.empty()) {
+				stringstream st(str);
 				string w;
 				vector<string> LSplit;
 				while (getline(st, w, ' ')) {
@@ -31,31 +31,29 @@ Map MapLoader::readMap(string mapFile) {
 				}
 				territoryList.push_back(LSplit[1]);
 				int continentIndex = stoi(LSplit[2]) - 1;
-				map.addTerritory(new Territory(LSplit[1], map.getContinentByIndex(continentIndex, continentsList)));
+				map.addTerritory(new Territory(LSplit[1], map.getContinents()[continentIndex]));
 			}
 			//moving to Borders
-			while (getline(dom, L) && L != "[borders]\r") {}
-			while (getline(dom, L) && L != "\r") {
-				stringstream st(L);
+			while (getline(file, str) && str != "[borders]") {}
+			while (getline(file, str) && !str.empty()) {
+				stringstream st(str);
 				string w;
 				vector<string> LSplit;
 				while (getline(st, w, ' ')) {
 					LSplit.push_back(w);
 				}
-				int territoryIndex = stoi(LSplit[0])-1;
 				for (int i = 1; i < LSplit.size(); i++) {
-					int indexToAdd = stoi(LSplit[i]) - 1;
-					map.getTerritoryByIndex(territoryIndex, territoryList)->addNeighbour(map.getTerritoryByIndex(indexToAdd, territoryList));
+				    map.getTerritories()[stoi(LSplit[0]) - 1]->addNeighbour(map.getTerritories()[stoi(LSplit[i]) - 1]);
 				}
 			}
 			if (map.validate()) {
+			    map.print();
 				return map;
 			}
 			else {
 				cerr << "This map is invalid!" << endl;
 				return 0;
 			}
-				
 
 		}
 		catch (...) {
