fffcbf7ebf32be164c48660735614336eed785f1
skyrimline <32933665+skyrimline@users.noreply.github.com>
2020-12-04 11:20:07 -0500

    Update MapLoader.h

--- MapLoader.h
+++ MapLoader.h
@@ -10,5 +10,36 @@ public:
     ~MapLoader();
 	Map* readMap(string mapFile);
 };
+class conquestFile {
+private:
+	vector<Continent*> continents;
+	vector<Territory*> territories;
+public:
+	conquestFile();
+	conquestFile(conquestFile* conFile);
+	~conquestFile();
+	vector<Continent*> getContinents();
+	vector<Territory*> getTerritories();
+	void addContinent(Continent* continent);
+	void addTerritory(Territory* territory);
+	void print();
+	bool isConnectedGraph();
+	bool validate();
+};
+class ConquestFileReader {
+public:
+	ConquestFileReader();
+	~ConquestFileReader();
+	Map* readConquestFile(string conquestFile);
+};
+class ConquestFileReaderAdapter : public MapLoader {
+private:
+	ConquestFileReader con;
+public:
+	ConquestFileReaderAdapter(ConquestFileReader cfreader) {
+	};
+
+	
+};
 
 #endif //MapLoader_H
