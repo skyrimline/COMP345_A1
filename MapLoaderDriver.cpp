a045a1605fa422e88e3dba1f2876806ef4529d98
skyrimline <32933665+skyrimline@users.noreply.github.com>
2020-12-04 11:22:06 -0500

    Update MapLoaderDriver.cpp

--- MapLoaderDriver.cpp
+++ MapLoaderDriver.cpp
@@ -7,10 +7,14 @@ int main() {
 	cout << "Hello" << endl;
 	cout << "Map is Loading......" << endl;
 	MapLoader Loader;
-    Map map1 = Loader.readMap("C:\\Users\\admin\\Desktop\\Comp\\Comp345\\Assignment1\\Map\\canada.map");
+	Map map1 = Loader.readMap("C:\\Users\\admin\\Desktop\\Comp\\Comp345\\Assignment1\\Map\\canada.map");
 	Map map2 = Loader.readMap("C:\\Users\\admin\\Desktop\\Comp\\Comp345\\Assignment1\\Map\\artic.map");
 	Map map3 = Loader.readMap("C:\\Users\\admin\\Desktop\\Comp\\Comp345\\Assignment1\\Map\\solar.map");
-    Map map4 = Loader.readMap("C:\\Users\\admin\\Desktop\\Comp\\Comp345\\Assignment1\\Map\\austria-hungary.map");
+	Map map4 = Loader.readMap("C:\\Users\\admin\\Desktop\\Comp\\Comp345\\Assignment1\\Map\\austria-hungary.map");
+
+	ConquestFileReader *cfReader = new ConquestFileReader();
+	ConquestFileReaderAdapter *adapter = new ConquestFileReaderAdapter(*cfReader);
+	MapLoader Loader2 = new ConquestFileReaderAdapter(*cfReader);
 
 	return 0;
 }
